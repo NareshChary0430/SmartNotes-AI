2b4d6445749f708282c3410846a72048c11843f0
NareshChary0430 <nareshchari300@gmail.com>
2026-06-17 01:53:09 +0530

    completed backend authentication using jwt

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './utils/connectDb.js';
+
+import authRouter from './routes/auth.route.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000; 
 
@@ -11,6 +23,8 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+
+app.use("/api/auth", authRouter);
 
 
 app.listen(PORT, () => {
