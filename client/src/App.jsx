bb20a8a40e4fd4286cea4ea4fa0e78139d70d460
NareshChary0430 <nareshchari300@gmail.com>
2026-07-01 00:02:34 +0530

    resolved all issues of .env and .gitignore files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,8 +33,6 @@ const App = () => {
       <Routes>
          <Route path='/' element={userData? <Home/> : <Navigate to="/auth" replace/>}/>
       <Route path='/auth' element={userData ? <Navigate to="/" replace/> : <Auth/>}/>
-    
-
       <Route path='/history' element={userData? <History/> : <Navigate to="/auth" replace/>}/>
       <Route path='/notes' element={userData? <Notes/> : <Navigate to="/auth" replace/>}/>
       <Route path='/pricing' element={userData? <Pricing/> : <Navigate to="/auth" replace/>}/>
